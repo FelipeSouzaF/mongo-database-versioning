40d38b6a5a587991a4623f11dd8ce4954ca43225
Felipe Farias <felipesouzaf11@gmail.com>
2021-06-10 23:16:52 -0300

    feat: implementado a execução dos arquivos de migration

--- src/commands/migrate.ts
+++ src/commands/migrate.ts
@@ -2,6 +2,8 @@ import {Command, flags} from '@oclif/command'
 import {Database} from '../config/database'
 import {MongoMigrateRc} from '../config/mongo-migrate-rc'
 import {migration} from '../models/migration'
+const glob = require('glob')
+import simpleGit from 'simple-git'
 
 export default class Migrate extends Command {
   static description = 'describe the command here'
@@ -30,18 +32,36 @@ export default class Migrate extends Command {
     try {
       // const {args, flags} = this.parse(Migrate)
 
+      const git = simpleGit()
+      const rootPath = await git.revparse(['--show-toplevel'])
+
       const mongoMigrateRc = new MongoMigrateRc()
       const configFile = await mongoMigrateRc.getConfigFile()
-      const {connection, connectionString} = configFile
 
-      const db = new Database({connection, connectionString})
+      const filesDir = configFile?.migrations.dir
+      // const {connection, connectionString} = configFile
+
+      const db = new Database(configFile)
       await db.connect()
 
-      const Migration = migration('migrations')
+      const MigrationModel = migration('migrations')
 
-      await Migration.findOne({})
+      const migrationFiles = await glob.sync(`${rootPath}/${filesDir}/*.js`)
+
+      for await (const migrationFile of migrationFiles) {
+        const Migration = require(migrationFile)
+        const migrationInstance = new Migration()
+
+        migrationInstance.run()
+
+        await MigrationModel.create({
+          fileName: migrationFile,
+          batch: 1,
+        })
+      }
 
       db.mongoose?.disconnect()
+
       this.log('run migration command')
     } catch (error) {
       this.error(error.message)
