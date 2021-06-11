83d7d67c0c60894207d5c8564397087dce340f52
Felipe Farias <felipesouzaf11@gmail.com>
2021-06-10 23:04:25 -0300

    refactor: alterado as propriedades das interfaces de tipos de database connection e mongo migrate rc

--- src/types/database-connection.d.ts
+++ src/types/database-connection.d.ts
@@ -1,10 +1,13 @@
 export interface DatabaseConnectionInterface {
-  host: string;
-  port: number;
-  user: string;
-  password: string;
-  database: string;
+  connectionString?: string;
+  connection?: {
+    host: string;
+    port: number;
+    user: string;
+    password: string;
+    database: string;
 
-  options: object;
-  debug: boolean;
+    options: object;
+    debug: boolean;
+  };
 }
--- src/types/mongo-migrate-rc.d.ts
+++ src/types/mongo-migrate-rc.d.ts
@@ -1,6 +1,18 @@
-import {DatabaseConnectionInterface} from './database-connection'
-
-export interface MongoMigrateRcInterface {
+export interface MongoMigrateRcInterface extends Object {
   connectionString?: string;
-  connection?: DatabaseConnectionInterface;
+  connection?: {
+    host: string;
+    port: number;
+    user: string;
+    password: string;
+    database: string;
+
+    options: object;
+    debug: boolean;
+  };
+
+  migrations: { dir: string; collectionName: string };
+  seeders: { dir: string };
+  factories: { dir: string };
+  tenants: { dir: string };
 }
